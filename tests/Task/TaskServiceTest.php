1f6d6565e0313973e0ffb6f28151767281ac01b9
Robert <mohammadhasan1379.79@gmail.com>
2025-08-06 15:43:56 +0330

    feat(tests.TaskServiceTest): test_get_task

--- tests/Task/TaskServiceTest.php
+++ tests/Task/TaskServiceTest.php
@@ -7,7 +7,6 @@ use App\Shared\HttpServer\Lib\Test\CoroutineTest;
 use App\Task\Application\Service\TaskService;
 use PHPUnit\Framework\TestCase;
 use Swoole\Http\Request;
-use Swoole\Http\Response;
 
 class TaskServiceTest extends TestCase
 {
@@ -22,6 +21,8 @@ class TaskServiceTest extends TestCase
         $request = new Request();
 
         $result = $service->createTask($request);
+
+        // check validation
         $this->assertEquals(HttpStatus::BAD_REQUEST, $result['code']);
 
         $this->runTestOnCoroutine(function () use ($service, $request) {
@@ -30,8 +31,10 @@ class TaskServiceTest extends TestCase
                 'description' => "bar",
             ];
             $result = $service->createTask($request);
-            $this->assertEquals(HttpStatus::CREATED, $result['code']);
             $task = $result['data'];
+
+            // check task returned
+            $this->assertEquals(HttpStatus::CREATED, $result['code']);
             $this->assertIsInt($task['id']);
             $this->assertEquals('foo', $task['title']);
             $this->assertEquals('bar', $task['description']);
@@ -40,4 +43,32 @@ class TaskServiceTest extends TestCase
         });
     }
 
+    public function test_get_task()
+    {
+        $this->runTestOnCoroutine(function () {
+            /**
+             * @var TaskService $service
+             */
+            $service = APP->get(TaskService::class);
+            $request = new Request();
+            $request->post = [
+                'title' => "foo",
+                'description' => "bar",
+            ];
+
+            // create task
+            $result = $service->createTask($request);
+            $task = $result['data'];
+
+            // get task
+            $result = $service->getTask($request);
+            $this->assertEquals(HttpStatus::BAD_REQUEST, $result['code']);
+            $request->get = [
+                'id' => $task['id'],
+            ];
+            $result = $service->getTask($request);
+            $this->assertEquals(HttpStatus::OK, $result['code']);
+            $this->assertEquals('foo', $result['data']['title']);
+        });
+    }
 }
