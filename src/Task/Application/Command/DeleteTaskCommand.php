1f0f433d40f6bbd794146ed6c42cdc24a501410e
Robert <mohammadhasan1379.79@gmail.com>
2025-08-07 14:36:47 +0330

    feat(Task.Command): DeleteTaskCommand

---
+++ src/Task/Application/Command/DeleteTaskCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Task\Application\Command;
+
+use App\Shared\App\Lib\Console\BaseCommand;
+use App\Shared\HttpServer\Lib\Client\Exception\ServerNotfoundException;
+use App\Shared\HttpServer\Lib\Client\HttpClient;
+use Swoole\Runtime;
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use function Swoole\Coroutine\run;
+
+class DeleteTaskCommand extends BaseCommand
+{
+    protected function getSignature(): string
+    {
+        return 'task:delete';
+    }
+
+    protected function configure()
+    {
+        $this->setDescription('delete task by id')
+            ->addArgument('id', InputArgument::REQUIRED, 'task id');
+    }
+
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        \Co\run(function () use ($input, $output): void {
+            \go(function () use ($input, $output): void {
+                try {
+                    $client = new HttpClient(env('HTTP_HOST'), env('HTTP_PORT'));
+                    $client->post('/task/delete', [
+                        'id' => $input->getArgument('id')
+                    ]);
+                    $output->writeln('deleted');
+                } catch (ServerNotfoundException $exception) {
+                    $output->writeln($exception->getMessage());
+                }
+            });
+        });
+        return Command::SUCCESS;
+    }
+
+}
